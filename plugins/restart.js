ba0faa9b3ede8b402b23d247a1fe579bdcc273a6
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-04 21:41:56 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const {exec} = require("child_process")
-reply("Ｗʜɪꜱᴘᴇʀ ᴹᴰ🧚‍♀️ɪꜱ Ｒᴇꜱᴛᴀʀᴛɪɴɢ...✨ 
+reply("Ｗʜɪꜱᴘᴇʀ ᴹᴰ🧚‍♀️ɪꜱ Ｒᴇꜱᴛᴀʀᴛɪɴɢ...✨ \n
       > Ｐᴏᴡᴇʀᴇᴅ Ｂʏ  Ｃʜᴀʀᴜᴋᴀ ᵀᴹ🧚‍♀️")
 await sleep(1500)
 exec("pm2 restart all")
