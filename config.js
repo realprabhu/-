a7848db41f0ab3cac8d99bc40ce2007ed52b8270
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-06 20:42:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_MSG: process.env.ALIVE_MSG || "", // YOUR MSG HERE
 ALIVE_IMG: process.env.ALIVE_IMG || "", // YOUR IMG HERE
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // PUT true OR false HERE..
+MODE: process.env.MODE || "inbox", // Use private , inbox , groups HERE..
 
 
 };
