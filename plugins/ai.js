05b96a39be235f93c003278e3bd14926071aced2
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-06 19:18:31 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,12 +1,13 @@
+const axios = require('axios');
+const { cmd } = require('../command');
 const { fetchJson } = require('../lib/functions');
-const {cmd} = require('../command');
 
+// Register the "npmsearch" command to search for npm packages
 cmd({
-  pattern: "ai",
-  desc: "ai chat bot",
-  category: "tools",
-  filename: __filename
-  
+    pattern: "ai",
+    desc: "Search for npm packages based on query",
+    category: "ai",  // Category for search-related commands
+    filename: __filename
 },
-    async(conn, mek, m,{from}
-)
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, reply }) => {
+    try {
