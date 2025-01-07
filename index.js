4c95024d0b9001e94be10d92aaf18b3032623863
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-07 19:43:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,9 +40,10 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+//connect mongodb
+const connectDB = require('./lib/mongodb')
+connectDB();
 
-//==============MONGODB======
-        
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
