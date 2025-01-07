1589c924b696bb202fa4901ce2991f2f904bbf6b
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-07 12:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
+const prefix = "."
 
 
 const ownerNumber = ['94789748241']
@@ -39,13 +40,6 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-//=============
-const connectDB = require('./lib/mongodb')
-connectDB();
-//=============
-const {readEnv } = require('./lib/database')
-const config = await readEnv();
-const prefix = config.PREFIX
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
