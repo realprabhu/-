e8ed00aeb8e92681f005911ce63a003e78af8553
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-07 19:26:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_MSG: process.env.ALIVE_MSG || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+SESSION_ID: process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "",
 };
