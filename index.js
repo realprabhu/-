76626a9c629701e90ff03427506ce85918dabe08
ʙʟᴀᴄᴋ ᴀʟᴘʜᴀ🧠🌏 <148836568+CharukaMahesh@users.noreply.github.com>
2025-01-07 22:38:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
+const prefix = ".";
 
 
 const ownerNumber = ['94789748241']
@@ -45,7 +46,6 @@ connectDB();
 //======================
 const {readEnv} = require('./lib/database')
 const config = await(readEnv);
-const prefix = config.PREFIX
 //========================
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
